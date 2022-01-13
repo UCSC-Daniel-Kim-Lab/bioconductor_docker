715f422922a05e92bbdb574dac6d3259f0376117
rregiar <rreggiar@ucsc.edu>
2022-01-12 20:12:44 -0800

    root = T and modular port

--- run_test.sh
+++ run_test.sh
@@ -8,9 +8,11 @@ FIGURES='/public/groups/kimlab/aale-KRAS-G12-transformation/figures:/home/${USER
 BIN='/public/groups/kimlab/aale-KRAS-G12-transformation/bin:/home/${USER}/bin'
 R='/public/groups/kimlab/aale-KRAS-G12-transformation/R:/home/${USER}/R'
 REFERENCE='/public/groups/kimlab/genomes.annotations/gencode.35:/home/$(USER)/reference'
-USER_ID=$(shell id -u)
+USER_ID=$(id -u)
 OUTPUT='/public/groups/kimlab/aale-KRAS-G12-transformation/output.data:/home/${USER}/output.data'
 CONFIG='/public/home/${USER}/.rstudio_docker_config:/home/${USER}/.config/rstudio'
+
+PORT=$1
 #-v ${DATA} \
 #-v ${NOTEBOOKS} \
 #-v ${FIGURES} \
@@ -19,10 +21,11 @@ CONFIG='/public/home/${USER}/.rstudio_docker_config:/home/${USER}/.config/rstudi
 #-v ${REFERENCE} \
 #-v ${OUTPUT} \
 
-@echo 'making rstudio session hosted at 127 0 0 1 2828 8787 for ${USER}:${USER_ID}'
-docker run --rm -p 127.0.0.1:3838:8787 -e DISABLE_AUTH=true \
+echo 'making rstudio session hosted at 127 0 0 1 "$PORT" 8787 for ${USER}:${USER_ID}'
+docker run --rm -p 127.0.0.1:"$PORT":8787 -e DISABLE_AUTH=true \
 	-e USER=${USER} \
 	-e USERID=${USER_ID} \
+	-e ROOT=TRUE \
 	--detach \
 	-v ${CONFIG} \
 	kimlab_rstudio:latest
