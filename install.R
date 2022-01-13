9e6e042ff121484e657a09c8e02fb57785c93eb7
rregiar <rreggiar@ucsc.edu>
2022-01-12 16:26:26 -0800

    update to custom package install

--- install.R
+++ install.R
@@ -1,5 +1,15 @@
 install.packages("BiocManager", repos="https://cran.rstudio.com")
 
+install.packages(c('here', 'ggsci', 'ggthemes', 'ggpubr', 'ggsignif',
+		   'ggforce', 'ggupset', 'patchwork', 'ggrepel',
+		   'extrafont', 'viridis', 'rjson', 'ggpmisc'))
+
 BiocManager::install(version="3.15", update=TRUE, ask=FALSE)
 
-BiocManager::install(c('devtools', 'AnVIL'))
+BiocManager::install(c('devtools', 'AnVIL', 'msigdbr', 'fgsea',
+		       'tximeta', 'summarizedExperiment', 'HDF5Array',
+		       'DESeq2', 'ComplexHeatmap', 'survival', 'survminer',
+		       'rGREAT', 'GenomicRanges', 'BSgenome.Hsapiens.UCSC.hg38',
+		       'BSgenome'))
+
+# RERWIP -- install r memes
