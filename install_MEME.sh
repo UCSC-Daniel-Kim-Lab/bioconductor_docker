3efd49dc9244a9173096a48bfd017090f0e22d9a
rregiar <rreggiar@ucsc.edu>
2023-04-07 17:36:52 -0700

    add sudo to make cmds and comments to script

--- install_MEME.sh
+++ install_MEME.sh
@@ -2,7 +2,7 @@ version=5.5.1
 wget http://meme-suite.org/meme-software/$version/meme-$version.tar.gz
 tar zxf meme-$version.tar.gz
 cd meme-$version
-./configure --prefix=$HOME/meme --with-url=http://meme-suite.org/ --enable-build-libxml2 --enable-build-libxslt
-make
-make test
-make install
+./configure --prefix=/opt/meme-5.5.1
+sudo make
+sudo make test
+sudo make install
