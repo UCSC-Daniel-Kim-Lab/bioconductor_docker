a339f54f944315812e56d6557a1099a489ba09f1
rregiar <rreggiar@ucsc.edu>
2023-04-07 17:53:21 -0700

    adding utils script for debian deps

---
+++ install_UTIL.sh
@@ -0,0 +1,6 @@
+# install common utils via Debian
+sudo apt-get install autoconf 
+sudo apt-get install automake 
+sudo apt-get install libtool
+sudo apt-get install zlib1g-dev
+sudo apt-get install libexpat1-dev
