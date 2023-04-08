dccfafbcdad7396d932f547d106427a97acb4b8b
rregiar <rreggiar@ucsc.edu>
2023-04-07 17:48:12 -0700

    MEME install make-up commit

--- install_MEME.sh
+++ install_MEME.sh
@@ -1,3 +1,7 @@
+# pulled from the website
+# downloads and installs MEME-suite to opt/meme-5.5.1
+# need to set this path in R; it is not default for {memes::}
+
 version=5.5.1
 wget http://meme-suite.org/meme-software/$version/meme-$version.tar.gz
 tar zxf meme-$version.tar.gz
